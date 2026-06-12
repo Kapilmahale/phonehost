a36a825df9d2ea6f456a906d0812a36cc32bbea4
Kapil Mahale <kapilmahale2@gmail.com>
2026-06-12 22:35:49 +0530

    test app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,8 @@
-import { BrowserRouter,Routes,Route,Navigate,} from "react-router-dom";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Deploy from "./pages/Deploy";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        <Route path="/login"element={<Login />}/>
-
-        <Route path="/register" element={<Register />}/>
-
-        <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Deploy />
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>PHONEHOST WORKING</h1>
+    </div>
   );
 }
 
