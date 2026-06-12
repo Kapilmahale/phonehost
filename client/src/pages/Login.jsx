6058bbcf3f4a5e180a70f0108af08ee97dd6aa1b
Kapil Mahale <kapilmahale2@gmail.com>
2026-06-12 23:26:46 +0530

    use state added to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function Login() {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+
   return (
     <div>
       <h1>LOGIN PAGE</h1>
+
+      <input
+        value={form.email}
+        onChange={(e) =>
+          setForm({
+            ...form,
+            email: e.target.value
+          })
+        }
+      />
     </div>
   );
 }
