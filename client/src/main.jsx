73a14c2529d615ab4cd69666b630bb8bc04e610f
Kapil Mahale <kapilmahale2@gmail.com>
2026-06-12 22:29:21 +0530

    Remove Google provider test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -9,10 +8,6 @@ createRoot(
   document.getElementById("root")
 ).render(
   <StrictMode>
-    <GoogleOAuthProvider
-      clientId="165429758878-d4hh5k7biuvp1i46fd2nt0isoq8d3rv7.apps.googleusercontent.com"
-    >
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>
 );
