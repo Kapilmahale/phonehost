c1256acb6728baa1e834d10805584abe5a4b9709
Kapil Mahale <kapilmahale2@gmail.com>
2026-06-12 23:07:56 +0530

    Login test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,7 @@
-import { BrowserRouter,Routes,Route,Navigate,} from "react-router-dom";
-
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Deploy from "./pages/Deploy";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        <Route path="/login"element={<Login />}/>
-
-        <Route path="/register" element={<Register />}/>
-
-        <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Deploy />
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
