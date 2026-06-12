1d91c63e2b9acea78b8dccdb006f1021f418dcef
Kapil Mahale <kapilmahale2@gmail.com>
2026-06-12 22:56:14 +0530

    original main.jsx and app.jsx code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -8,6 +9,10 @@ createRoot(
   document.getElementById("root")
 ).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider
+      clientId="165429758878-d4hh5k7biuvp1i46fd2nt0isoq8d3rv7.apps.googleusercontent.com"
+    >
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
